3cee24519c48c50fa5f1fd71f4526ae448e5a11a
Paul Harris <pirh.badger@gmail.com>
2012-09-10 15:18:19 +0100

    Renamed module.

--- AllAboutMonstrs/AllAboutMonstrs/gui.py
+++ AllAboutMonstrs/AllAboutMonstrs/gui.py
@@ -3,7 +3,7 @@ from pygame.transform import flip,scale
 import chromographs
 import typefaces
 
-class TextBox(object):
+class TextFrame(object):
     head_end = chromographs.obtain("flourish/top-end.png")
     head_mid = chromographs.obtain("flourish/top-mid.png")
     head_line = chromographs.obtain("flourish/top-line.png")
@@ -14,10 +14,10 @@ class TextBox(object):
                  fontsize="normal",colour=(0,0,0)):
         self.position = position
         try:
-            self.passage = typefaces.prepare_passage(
+            self.content = typefaces.prepare_passage(
                 text_or_surface,width,size=fontsize,colour=colour)
         except AttributeError:
-            self.passage = text_or_surface
+            self.content = text_or_surface
         self.prepare_size(width)
 
     def prepare_size(self,asked_width):
@@ -28,19 +28,19 @@ class TextBox(object):
         fl = self.foot_line
         self.min_header_width = mhw = he.get_width()*2 + hm.get_width()
         self.min_footer_width = mfw = fe.get_width()*2
-        self.width = max(asked_width,self.passage.get_width(),mhw,mfw)
+        self.width = max(asked_width,self.content.get_width(),mhw,mfw)
         topgap = (self.width-self.min_header_width)/2
         bottomgap = self.width-self.min_footer_width
         self.top_line = scale(hl,(topgap,hl.get_height()))
         self.bottom_line = scale(fl,(bottomgap,fl.get_height()))
         self.headheight = he.get_height()
         self.footheight = fe.get_height()
-        self.indent = (self.width - self.passage.get_width())/2
+        self.indent = (self.width - self.content.get_width())/2
 
     def render(self,screen):
         x,y = self.position
         self.render_header(screen)
-        screen.blit(self.passage,(x+self.indent, y+self.headheight))
+        screen.blit(self.content,(x+self.indent, y+self.headheight))
         self.render_footer(screen)
 
     def render_header(self,screen):
@@ -56,7 +56,7 @@ class TextBox(object):
 
     def render_footer(self,screen):
         x,y = self.position
-        y += self.headheight+self.passage.get_height()
+        y += self.headheight+self.content.get_height()
         ew = self.foot_end.get_width()
         lw = self.bottom_line.get_width()
         screen.blit(self.foot_end,(x,y))
