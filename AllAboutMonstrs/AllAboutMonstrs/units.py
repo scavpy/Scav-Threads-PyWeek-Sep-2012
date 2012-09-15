efc3750460aaaa0222218132761f95fd917196e6
scav <pete.alex.harris@gmail.com>
2012-09-15 18:34:32 +0100

    Improved appearance of cannon and dead trinitroceratops

--- AllAboutMonstrs/AllAboutMonstrs/units.py
+++ AllAboutMonstrs/AllAboutMonstrs/units.py
@@ -222,7 +222,7 @@ class Cannon(Unit):
     animated_chromograph_name = "units/cannon.png"
     walking_animations = 1
     attacking_animations = 2
-    orientation_indices = (2,1,1,1,3,0,0,0)
+    orientation_indices = (0,1,2,3,4,5,6,7)
     footprint = 20,16
     area_of_awareness = (200,160)
     area_of_attack = (80, 64)
